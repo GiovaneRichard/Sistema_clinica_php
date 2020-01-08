504fb6638eede55c2ac29b687e9b206ef553f91b
Giovane Richard <giovanerichard@gmail.com>
2020-01-08 20:55:18 -0300

    Busca dos usuários

--- cursophp.com/App/views/cadUsuario.php
+++ cursophp.com/App/views/cadUsuario.php
@@ -22,6 +22,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link rel="stylesheet" href="../../css/bulma.min.css" />
+
 </head>
 <body class="hold-transition register-page">
 <div class="register-box">
